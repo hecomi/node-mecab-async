42facecfac350489486720e7731c44dd5fc52c80
beeant (macbn) <beeant@gmail.com>
2013-10-08 02:06:52 +0900

    add parseFormat, format result into object with explanation keys

--- mecab.js
+++ mecab.js
@@ -1,8 +1,29 @@
-var exec     = require('child_process').exec;
+var exec = require('child_process').exec;
 
 // 後方互換のため
 var MeCab = function() {};
 MeCab.prototype = {
+	_format: function(parsedResult, callback) {
+		var result = [];
+		// Reference: http://mecab.googlecode.com/svn/trunk/mecab/doc/index.html
+		// 表層形\t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
+		parsedResult.forEach(function(parsed) {
+			if(parsed.length <= 8) { return; }
+			result.push({
+				kanji: parsed[0],
+				lexical: parsed[1],
+				compound: parsed[2],
+				compound2: parsed[3],
+				compound3: parsed[4],
+				conjugation: parsed[5],
+				inflection: parsed[6],
+				original: parsed[7],
+				reading: parsed[8],
+				pronunciation: parsed[9] || ''
+			});
+		});
+		callback(null, result);
+	},
 	_parseMeCabResult : function(result) {
 		var ret = [];
 		result.split('\n').forEach(function(line) {
@@ -11,12 +32,17 @@ MeCab.prototype = {
 		return ret;
 	},
 	parse : function(str, callback) {
-		exec('echo ' + str + ' | mecab', function(err, result) {
+		exec('echo "' + str + '" | mecab', function(err, result) {
 			if (err) {
 				callback(err, null);
 				return;
 			}
 			callback(null, MeCab._parseMeCabResult(result).slice(0,-2));
+		});
+	},
+	parseFormat: function(str, callback) {
+		MeCab.parse(str, function(err, result) {
+			MeCab._format(result, callback);
 		});
 	},
 	_wakatsu : function(arr) {
