951d3386df85ab07212c1c7ed155d6dcab66c2e3
Hisateru Tanaka <tanakahisateru@gmail.com>
2014-08-20 18:23:42 +0900

    コマンドラインに渡す文字列をエスケープ

--- test.js
+++ test.js
@@ -1,30 +1,40 @@
 var MeCab = new require('./mecab.js');
-var testWord = 'すもももももももものうち';
 
-// 非同期版
-MeCab.parse(testWord, function(err, result) {
-	if (err) throw err;
-	console.log('非同期:\n', result);
-});
+runAllFeatures('すもももももももものうち');
 
-// 非同期版
-MeCab.wakachi(testWord, function(err, result) {
-	if (err) throw err;
-	console.log('非同期（わかち書き）:\n', result);
-});
+runPartialFeature('\'"\\');
+runPartialFeature('&\necho abc');
+runPartialFeature('abc | cat');
 
-// 非同期版
-MeCab.parseFormat(testWord, function(err, result) {
-	if (err) throw err;
-	console.log('非同期（フォーマット整形）:\n', result);
-});
+function runAllFeatures(testWord) {
+    // 非同期版
+    MeCab.parse(testWord, function (err, result) {
+        if (err) throw err;
+        console.log('非同期:\n', result);
+    });
 
-// 同期版
-console.log('同期:\n', MeCab.parseSync(testWord) );
+    // 非同期版
+    MeCab.wakachi(testWord, function (err, result) {
+        if (err) throw err;
+        console.log('非同期（わかち書き）:\n', result);
+    });
 
-// 同期版
-console.log('同期（わかち書き）:\n', MeCab.wakachiSync(testWord) );
+    // 非同期版
+    MeCab.parseFormat(testWord, function (err, result) {
+        if (err) throw err;
+        console.log('非同期（フォーマット整形）:\n', result);
+    });
 
-// 同期版
-console.log('同期（フォーマット整形）:\n', MeCab.parseSyncFormat(testWord) );
+    // 同期版
+    console.log('同期:\n', MeCab.parseSync(testWord));
 
+    // 同期版
+    console.log('同期（わかち書き）:\n', MeCab.wakachiSync(testWord));
+
+    // 同期版
+    console.log('同期（フォーマット整形）:\n', MeCab.parseSyncFormat(testWord));
+}
+
+function runPartialFeature(testWord) {
+    console.log("わかち書き " + testWord + " :\n", MeCab.wakachiSync(testWord));
+}
