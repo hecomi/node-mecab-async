f91245cc5cb641acdf9ec06fc2d7bed3d11185b8
hecomi <info@hecomi.com>
2017-10-28 16:43:33 +0900

    コード修正

--- mecab.js
+++ mecab.js
@@ -1,6 +1,6 @@
-var exec     = require('child_process').exec;
-var execSync = require('child_process').execSync;
-var sq       = require('shell-quote');
+const exec     = require('child_process').exec;
+const execSync = require('child_process').execSync;
+const sq       = require('shell-quote');
 
 // for backward compatibility
 var MeCab = function() {};
@@ -9,11 +9,11 @@ MeCab.prototype = {
     command : 'mecab',
     options : {},
     _format : function(arrayResult) {
-        var result = [];
+        const result = [];
         if (!arrayResult) { return result; }
         // Reference: http://mecab.googlecode.com/svn/trunk/mecab/doc/index.html
         // 表層形\t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
-        arrayResult.forEach(function(parsed) {
+        arrayResult.forEach(parsed => {
             if (parsed.length <= 8) { return; }
             result.push({
                 kanji         : parsed[0],
@@ -34,8 +34,8 @@ MeCab.prototype = {
         return sq.quote(['echo', str]) + ' | ' + this.command;
     },
     _parseMeCabResult : function(result) {
-        return result.split('\n').map(function(line) {
-            var arr = line.split('\t');
+        return result.split('\n').map(line => {
+            const arr = line.split('\t');
             // EOS
             if (arr.length === 1) {
                 return [line];
@@ -44,19 +44,19 @@ MeCab.prototype = {
         });
     },
     parse : function(str, callback) {
-        process.nextTick(function() { // for bug
-            exec(MeCab._shellCommand(str), this.options, function(err, result) {
+        process.nextTick(() => { // for bug
+            exec(MeCab._shellCommand(str), this.options, (err, result) => {
                 if (err) { return callback(err); }
                 callback(err, MeCab._parseMeCabResult(result).slice(0,-2));
             });
         });
     },
     parseSync : function(str) {
-        var result = execSync(MeCab._shellCommand(str), this.options);
+        const result = execSync(MeCab._shellCommand(str), this.options);
         return MeCab._parseMeCabResult(String(result)).slice(0, -2);
     },
     parseFormat : function(str, callback) {
-        MeCab.parse(str, function(err, result) {
+        MeCab.parse(str, (err, result) => {
             if (err) { return callback(err); }
             callback(err, MeCab._format(result));
         });
@@ -65,21 +65,21 @@ MeCab.prototype = {
         return MeCab._format(MeCab.parseSync(str));
     },
     _wakatsu : function(arr) {
-        return arr.map(function(data) { return data[0]; });
+        return arr.map(data => data[0]);
     },
     wakachi : function(str, callback) {
-        MeCab.parse(str, function(err, arr) {
+        MeCab.parse(str, (err, arr) => {
             if (err) { return callback(err); }
             callback(null, MeCab._wakatsu(arr));
         });
     },
     wakachiSync : function(str) {
-        var arr = MeCab.parseSync(str);
+        const arr = MeCab.parseSync(str);
         return MeCab._wakatsu(arr);
     }
 };
 
-for (var x in MeCab.prototype) {
+for (const x in MeCab.prototype) {
     MeCab[x] = MeCab.prototype[x];
 }
 
